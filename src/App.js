c6299bdf01417ad2bbc7eadfd35f723b86aa5126
Raju Kumar <rajukumar20182018@gmail.com>
2024-08-11 23:42:25 +0530

    raju

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const[name,setname]=useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <label For="name">Enter your name:</label>
+      <br/>
+      <input type="text" id='name' value={name} onChange={(e)=>setname(e.target.value)} />
+      {name && <p>Hello, {name}!</p>}
+     
     </div>
   );
 }
